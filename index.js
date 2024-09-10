f402e977d9604fcda7735f9a0b3289428b9a855f
EduardoLorensettiLopes <eduardolorensetti@gmail.com>
2024-09-10 17:26:00 -0300

    update port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/usuarios/:id', async (req, res) => {
     const users = await prisma.user.findUnique({
